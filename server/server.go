ba032b3117e2821b812ccdeacb0abbce9b629a17
FUKUNAGA Shunya <fukushun1994@gmail.com>
2022-08-13 19:15:44 +0900

    fix to add join process

--- server/server.go
+++ server/server.go
@@ -12,12 +12,19 @@ import (
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/youdofoo/grpc-chat/pb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type room struct {
 	name     string
-	messages []string
-	streams  []pb.Chat_ChatServer
+	messages []*pb.ChatMessage
+	users    map[string]*user
+}
+
+type user struct {
+	name   string
+	stream pb.Chat_ChatServer
 }
 
 type chatServer struct {
@@ -44,27 +51,95 @@ func (s *chatServer) GetRooms(ctx context.Context, in *empty.Empty) (*pb.Rooms, 
 }
 
 func (s *chatServer) Chat(stream pb.Chat_ChatServer) error {
+	var roomName string
+	var userName string
 	for {
 		msg, err := stream.Recv()
 		if err == io.EOF {
 			// Client leaved
+			s.leave(roomName, userName)
 			return nil
 		}
-		r := s.lookupRoom(msg.RoomName)
-		if r == nil {
-			r = s.createRoom(msg.RoomName, stream)
+		if err != nil {
+			s.leave(roomName, userName)
+			return err
 		}
-		s.mu.Lock()
-		r.messages = append(r.messages, msg.Text)
-		for _, stream := range r.streams {
-			err = stream.Send(&pb.ChatMessage{Text: msg.Text})
+		switch msg.Type {
+		case pb.ChatMessageType_JOIN:
+			roomName = msg.RoomName
+			userName = msg.UserName
+			err = s.join(roomName, userName, stream)
 			if err != nil {
-				s.mu.Unlock()
+				return err
+			}
+		case pb.ChatMessageType_SEND:
+			if msg.RoomName != roomName || msg.UserName != userName {
+				// ignore invalid room or user
+				log.Println("sent room name or user name is wrong")
+				continue
+			}
+			err = s.send(msg)
+			if err != nil {
 				return err
 			}
 		}
-		s.mu.Unlock()
 	}
+}
+
+func (s *chatServer) join(roomName, userName string, stream pb.Chat_ChatServer) error {
+	r := s.lookupRoom(roomName)
+	if r == nil {
+		r = s.createRoom(roomName, stream)
+	}
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if _, ok := r.users[userName]; ok {
+		return status.Errorf(codes.AlreadyExists, "user %s already exists in room %s", userName, roomName)
+	}
+	r.users[userName] = &user{
+		name:   userName,
+		stream: stream,
+	}
+
+	// send all past messages
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	for _, msg := range r.messages {
+		err := stream.Send(msg)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *chatServer) send(msg *pb.ChatMessage) error {
+	r := s.lookupRoom(msg.RoomName)
+	if r == nil {
+		return status.Errorf(codes.NotFound, "room %s not found", msg.RoomName)
+	}
+	s.mu.Lock()
+	r.messages = append(r.messages, msg)
+	for _, user := range r.users {
+		err := user.stream.Send(msg)
+		if err != nil {
+			s.mu.Unlock()
+			return err
+		}
+	}
+	s.mu.Unlock()
+	return nil
+}
+
+func (s *chatServer) leave(roomName, userName string) {
+	r := s.lookupRoom(roomName)
+	if r == nil {
+		return
+	}
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(r.users, userName)
 }
 
 func (s *chatServer) lookupRoom(name string) *room {
@@ -77,8 +152,8 @@ func (s *chatServer) createRoom(name string, stream pb.Chat_ChatServer) *room {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	s.rooms[name] = &room{
-		name:    name,
-		streams: []pb.Chat_ChatServer{stream},
+		name:  name,
+		users: make(map[string]*user),
 	}
 	return s.rooms[name]
 }
